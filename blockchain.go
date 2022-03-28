a406b1b441908780b18511502cb149bf42c15c60
stinekamau <paulkamau@gmail.com>
2022-03-28 04:12:59 +0300

    Created the core ds

---
+++ blockchain.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+type Block struct {
+	nonce        int
+	transactions []string
+	previousHash string
+	timestamp    int64
+}
+
+func (b *Block) Print() {
+	fmt.Printf("Previous Hash: %s\n", b.timestamp)
+	fmt.Printf("Nonce: %d\n", b.nonce)
+	fmt.Printf("Timestamp : %d\n", b.timestamp)
+	fmt.Printf("Transactions: %s\n", b.transactions)
+}
+
+// BlockChain Creation of the blockchain
+type BlockChain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func (bc *BlockChain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+func (bc *BlockChain) Chain() []*Block {
+	return bc.chain
+}
+
+func NewBlockChain() *BlockChain {
+	bc := BlockChain{}
+	bc.CreateBlock(0, "init hash")
+
+	return &bc
+
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	return &Block{
+		timestamp:    time.Now().UnixNano(),
+		nonce:        nonce,
+		previousHash: previousHash,
+	}
+}
+
+func (bc *BlockChain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("Chain %d \n", i)
+		block.Print()
+	}
+}
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+func main() {
+	bc := NewBlockChain()
+	bc.CreateBlock(5, "hash 1")
+	bc.CreateBlock(6, "hash 2")
+	bc.Print()
+}
