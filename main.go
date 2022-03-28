1e71f9a1757990a9cdcf91acfb0e605318babd0e
stinekamau <paulkamau@gmail.com>
2022-03-28 08:09:51 +0300

    Wallet correctly working

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
-package blockchain
+package main
+
+import (
+	wallet2 "blockchain/wallet"
+	"fmt"
+)
+
+func main() {
+
+	wallet := wallet2.NewWallet()
+	fmt.Println(wallet.PublicKeyStr())
+	fmt.Println(wallet.PublicKeyStr())
+
+}
